1acde3897ed7744c0c28e6935750cb925de18b42
Liubov Kushko <lyuba-0258@yandex.ru>
2023-07-21 13:12:50 +0700

    hotfix: включить встроенную валидацию

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm({name, title, buttonText, isOpen, onClose, onSubmit, chil
         <div className="popup__container popup__wrapper">
           <button className="button button_type_close" type="button" onClick={onClose}></button>
           <h2 className="popup__title">{title}</h2>
-          <form className="form" name={`${name}-form`} onSubmit={onSubmit} noValidate>
+          <form className="form" name={`${name}-form`} onSubmit={onSubmit} >
             {children}
             <button className="button button_type_submit" type="submit">{buttonText}</button>
           </form>
